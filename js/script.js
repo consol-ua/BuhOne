bd3efcf438a3303019ffb68f5c5a0056949a25a2
Malyhin Ivan <malygin91@gmail.com>
2021-01-17 17:32:01 +0200

    +sliderBanner

--- js/script.js
+++ js/script.js
@@ -36,5 +36,59 @@ function burgerClick() {
   burgerEl.addEventListener("click", addClass);
 }
 
+function sliderBanner() {
+  const arrowRight = 'arrow__right';
+  const arrowLeft = 'arrow__left'
+  const point = 'point__item'
+  const root = document.querySelector('.banner__button')
+  const arrayBanner = [];
+  const bannerSliderElement = document.querySelector('#slider-banner').children
+  const arrayPoint = [];
+  const pointElement = document.querySelector('.point').children;
 
+
+  for (let i = 0; i < bannerSliderElement.length; i++) {
+    arrayBanner.push(bannerSliderElement[i])
+  }
+
+  for (let i = 0; i < pointElement.length; i++) {
+    arrayPoint.push(pointElement[i])
+  }
+  function button(target) {
+    let indexActivePoint = arrayPoint.findIndex((el) => el.className.includes('point__item--activ'))
+    let indexActiveBanner = arrayBanner.findIndex((el) => el.className.includes('screen-activ'))
+
+    if (target.className == (arrowRight)) {
+      bannerSliderElement[indexActiveBanner].className = 'screen screen-no-activ'
+      bannerSliderElement[indexActivePoint + 1].className = 'screen screen-activ';
+      pointElement[indexActivePoint].className = 'point__item';
+      pointElement[indexActiveBanner + 1].className = 'point__item point__item--activ';
+    } else if (target.className == (arrowLeft)) {
+      bannerSliderElement[indexActiveBanner].className = 'screen screen-no-activ'
+      bannerSliderElement[indexActivePoint - 1].className = 'screen screen-activ';
+      pointElement[indexActivePoint].className = 'point__item';
+      pointElement[indexActiveBanner - 1].className = 'point__item point__item--activ';
+    } else if (target.className.includes(point)) {
+      let indexPoint = arrayPoint.indexOf(target);
+      bannerSliderElement[indexActiveBanner].className = 'screen screen-no-activ'
+      bannerSliderElement[indexPoint].className = 'screen screen-activ';
+      pointElement[indexActivePoint].className = 'point__item';
+      pointElement[indexPoint].className = 'point__item point__item--activ';
+    }
+
+    if (bannerSliderElement[bannerSliderElement.length - 1].className.includes('screen-activ')) {
+      root.querySelector('.arrow__right').className = arrowRight + ' ' + 'arrow__last-el'
+    } else {
+      root.querySelector('.arrow__right').className = arrowRight
+    }
+    if (bannerSliderElement[0].className.includes('screen-activ')) {
+      root.querySelector('.arrow__left').className = arrowLeft + ' ' + 'arrow__last-el'
+    } else {
+      root.querySelector('.arrow__left').className = arrowLeft
+    }
+  }
+  root.addEventListener('click', (e) => button(e.target))
+}
+
+sliderBanner();
 burgerClick();
